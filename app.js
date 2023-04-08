443cd7ce1d26c3a31067077af637f808c53fb96f
Rafael <rcarmona50@gmail.com>
2023-04-07 23:01:03 -0700

    Added overlay to image.

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const observer = new IntersectionObserver((entries) =>{
     
 });
 
-
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el))
+
+
